a2d9ccbcfa55746a7fa9d67f5aa53e9248c044f8
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-13 12:07:20 +0300

    12-й коммит, urls

--- lms/urls.py
+++ lms/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from lms.apps import LmsConfig
-from lms.views import CourseViewSet, LessonCreateApiView, LessonListApiView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+from lms.views import CourseViewSet, LessonCreateApiView, LessonListApiView, LessonRetrieveAPIView, LessonUpdateAPIView, \
+    LessonDestroyAPIView
 
 app_name = LmsConfig.name
 
@@ -10,11 +11,11 @@ router = SimpleRouter()
 router.register('', CourseViewSet)
 
 urlpatterns = [
-    path('lesson/', LessonListApiView.as_view()),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view()),
-    path('lesson/create/', LessonCreateApiView.as_view()),
-    path('lesson/<int:pk>/destroy/', LessonDestroyAPIView.as_view()),
-    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view())
+    path('lesson/', LessonListApiView.as_view(), name='Lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='Lesson_retrieve'),
+    path('lesson/create/', LessonCreateApiView.as_view(), name='Lesson_create'),
+    path('lesson/<int:pk>/destroy/', LessonDestroyAPIView.as_view(), name='Lesson_destroy'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='Lesson_update'),
 ]
 
 urlpatterns += router.urls
