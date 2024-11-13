c7378fe2e69784b918df4e6061efac0eae59da8f
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-13 10:19:14 +0300

    7-й коммит, создание модели пользователя

--- users/models.py
+++ users/models.py
@@ -1,4 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 
 # Create your models here.
 
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="уакжите e-mail"
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="номер телефона",
+        help_text="укажите номер телефона",
+    )
+    city = models.CharField(
+        max_length=50, verbose_name="город", help_text="укажите город"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="загружите фото",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
