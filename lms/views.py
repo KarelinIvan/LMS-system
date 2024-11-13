146a895dcfff86f4a7e57325d9c98d98be9ced12
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-13 12:37:09 +0300

    13-й коммит, urls

--- lms/views.py
+++ lms/views.py
@@ -1,4 +1,10 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
