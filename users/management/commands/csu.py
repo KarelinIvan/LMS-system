405d34db0763e2dec4601b35c055c171d9d3a164
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-17 19:00:27 +0300

    22-й коммит миграция

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,9 +5,13 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email="ivan.karelin.1993@mail.ru")
+        user = User.objects.create(
+            email="ivan.karelin.1993@mail.ru",
+            first_name='Admin',
+            is_superuser=True,
+            is_active=True,
+            is_staff=True,
+        )
+
         user.set_password("Mt076954")
-        user.is_active = True
-        user.is_staff = True
-        user.is_superuser = True
         user.save()
