ddc341b83beee6e78f52d829bf53025d7f7eddb1
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-18 20:19:33 +0300

    35-й коммит, докстринг

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -4,6 +4,7 @@ from users.models import User
 
 
 class Command(BaseCommand):
+    """Кастомная команда для создания суперпользователя"""
     def handle(self, *args, **options):
         user = User.objects.create(
             email="ivan.karelin.1993@mail.ru",
