7d2896f2a9778ffc7dd97218cc68ae05366716f6
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-18 20:24:54 +0300

    36-й коммит, докстринг

--- users/admin.py
+++ users/admin.py
@@ -5,9 +5,11 @@ from users.models import User, Payment
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', )
+    """Понель пользователи  в админке"""
+    list_display = ('email', 'id', 'phone')
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
+    """Панель платежи в админке"""
     list_display = ('user', 'payment_date', 'paid_course', 'paid_lesson', 'amount', 'payment_method')
