a86e1baee6a1ca9f961ff75fc13e335180d73e3f
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-11-28 22:01:51 +0300

    47-й коммит, тесты

--- lms/tests.py
+++ lms/tests.py
@@ -6,7 +6,7 @@ from lms.models import Course, Lesson
 from users.models import User
 
 
-class CourseTestCase(APITestCase):
+class LessonTestCase(APITestCase):
     def setUp(self):
         """ Создание пользователя, курса, урока"""
 
@@ -14,12 +14,49 @@ class CourseTestCase(APITestCase):
         self.course = Course.objects.create(title="Test Course")
         self.lesson = Lesson.objects.create(title='Test Lesson')
         self.client.force_authenticate(user=self.user)
-        self.course.lesson_set.add(self.lesson)
 
-    def test_course_create(self):
-        """Тестирование созданеия курса"""
-        url = reverse('lms:course-list')
+    def test_lesson_create(self):
+        """ Тестирование созданеия урока """
+        url = reverse("lms:Lesson_create")
         data = {"title": "test", "description": "test"}
         response = self.client.post(url, data=data)
+        data = response.json()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Course.objects.all().count(), 2)
+        self.assertEqual(data.get("title"), "test")
+
+    def test_lessons_list(self):
+        """ Тестирование отображения уроков """
+
+        url = reverse('lms:Lesson_list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.data), 0)
+
+    def test_lesson_retrieve(self):
+        """ Тестирование отображения урока """
+
+        url = reverse('lms:Lesson_retrieve', args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("title"), self.lesson.title)
+
+    def test_lesson_update(self):
+        """ Тестирование редактирования урока """
+
+        url = reverse('lms:Lesson_update', args=(self.lesson.pk,))
+        data = {"title": "Updated test", "description": "update test"}
+        response = self.client.put(url, data)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("title"), "Updated test")
+
+    def test_lesson_destroy(self):
+        """ Тестирование удаления урока """
+
+        url = reverse('lms:Lesson_destroy', args=(self.lesson.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Lesson.objects.count(), 0)
