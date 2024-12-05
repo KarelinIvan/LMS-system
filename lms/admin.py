bc97c2fafcb26d40c76578b2cf68139db57d2b7b
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-12-05 20:55:44 +0300

    97-й коммит, отоброжение моделей в админке

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from lms.models import Course, Lesson, Subscription
+
+
+@admin.register(Course)
+class UserCourse(admin.ModelAdmin):
+    list_display = ("id", "title", "preview", "description", "owner")
+
+
+@admin.register(Lesson)
+class UserLesson(admin.ModelAdmin):
+    list_display = ("id", "title", "course", "description", "preview", "link_to_video", "owner")
+
+
+@admin.register(Subscription)
+class UserSubscription(admin.ModelAdmin):
+    list_display = ("id", "user", "course")
